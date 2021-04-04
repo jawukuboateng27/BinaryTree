6cc043bbf8e6b4dbf7b39cd2bacd3b3aed33b34d
jawukuboateng27 <59456830+jawukuboateng27@users.noreply.github.com>
2021-04-03 20:49:49 -0400

    Update BinaryTree.h

--- BinaryTree.h
+++ BinaryTree.h
@@ -4,40 +4,51 @@
 using namespace std;
 
 template<class T>
-struct Node
+struct NodeType
 {
     T key;
-    Node<T> *left;
-    Node<T> *right;
+    NodeType<T> *left;
+    NodeType<T> *right;
 
 };
 
 template<class T>
 class BinaryTree {
-    struct Node<T> *root;
+    struct NodeType<T> *root;
 
 public:
 BinaryTree();
 ~BinaryTree();
+void clear(NodeType<T> *node);
 void insert(T &key);
-void deleteItm(T &key);
+  
+void deleteItem(T key);
+void deleteNode(NodeType<T>*& targetNode);
+T getPredecessor(NodeType<T>*& node) const;
+bool searchAndDelete(NodeType<T> *&node, T key);
+
+  
 void retrieve(T &item, bool &found)const;
-void preOrderTraversal(Node<T> *node) const;
+void preOrderTraversal(NodeType<T> *node) const;
 void preOrder()const;
-void inOrderTraversal(Node<T> *node) const;
+void inOrderTraversal(NodeType<T> *node) const;
 void inOrder()const;
-void postOrderTraversal(Node<T> *node) const;
+void postOrderTraversal(NodeType<T> *node) const;
 void postOrder()const;
 int getLength()const;
-void initialize(int number);
+int calculateLength(NodeType<T> *node)const;
+void initialize(T number);
+
+  
 //check return type
 void getNumSingleParent();
 void getNumLeafNodes();
-void getSumOfSubtrees();
+int getSumOfSubtrees(T &element);
+
 
 private:
     int length;
-    int value;
+    T value;
 
 
 };
